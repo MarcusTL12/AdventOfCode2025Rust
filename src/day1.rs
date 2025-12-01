b9ab114bb78a695e7e1e82d8dcb890141fe542b9
MarcusTL12 <marcuspaafjellet@gmail.com>
2025-12-01 06:11:18 +0100

    day 1 part 1

--- src/day1.rs
+++ src/day1.rs
@@ -3,7 +3,32 @@ use crate::{Day, TaskResult};
 pub const PARTS: Day = [part1, part2];
 
 fn part1(input: String) -> TaskResult {
-    todo!("{input}")
+    let ans = input
+        .lines()
+        .map(|l| {
+            let rl = l.chars().next().unwrap();
+
+            let n: i32 = l[1..].parse().unwrap();
+
+            (rl, n)
+        })
+        .scan(50, |dial, (rl, n)| {
+            *dial = match rl {
+                'R' => *dial + n,
+                'L' => *dial - n,
+                _ => panic!(),
+            } % 100;
+
+            if *dial < 0 {
+                *dial += 100;
+            }
+
+            Some(*dial)
+        })
+        .filter(|&x| x == 0)
+        .count();
+
+    ans.into()
 }
 
 fn part2(input: String) -> TaskResult {
