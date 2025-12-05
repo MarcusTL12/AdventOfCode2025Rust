bd3e54ec0e2027c1fb5b676118251428fedeb463
MarcusTL12 <marcuspaafjellet@gmail.com>
2025-12-05 09:45:41 +0100

    part 2

--- src/day5.rs
+++ src/day5.rs
@@ -1,5 +1,3 @@
-use std::ops::RangeInclusive;
-
 use crate::{Day, TaskResult};
 
 pub const PARTS: Day = [part1, part2];
@@ -25,39 +23,48 @@ fn part1(input: String) -> TaskResult {
     ans.into()
 }
 
-fn combine_ranges(
-    [a0, a1]: [u64; 2],
-    [b0, b1]: [u64; 2],
-) -> ([u64; 2], Option<[u64; 2]>) {
-    if a1 + 1 < b0 {
-        ([a0, a1], Some([b0, b1]))
-    } else if b1 + 1 < a0 {
-        ([b0, b1], Some([a0, a1]))
-    } else {
-        ([a0.min(b0), a1.max(b1)], None)
-    }
-}
-
 fn insert_new_range(ranges: &mut Vec<[u64; 2]>, [a0, a1]: [u64; 2]) {
     if ranges.is_empty() {
         ranges.push([a0, a1]);
         return;
     }
 
-    let mut lo = 0;
-    let mut hi = ranges.len() - 1;
+    let mut i = 0;
 
-    while lo < hi {
-        let mid = lo + (hi - lo) / 2;
+    while let Some(&[_, b1]) = ranges.get(i)
+        && a0 > b1 + 1
+    {
+        i += 1;
+    }
 
-        let [m0, m1] = ranges[mid];
+    let mut j = i;
 
-        if m1 + 1 < a0 {
-            lo = mid + 1;
-        } else if a1 + 1 < m0 {
-            hi = mid - 1;
+    while let Some(&[_, b1]) = ranges.get(j)
+        && a1 > b1 + 1
+    {
+        j += 1;
+    }
+
+    if i >= ranges.len() {
+        ranges.push([a0, a1]);
+    } else {
+        let new0 = ranges[i][0].min(a0);
+
+        let (in_right, new1) = if let Some(&[h0, h1]) = ranges.get(j)
+            && h0 <= a1 + 1
+        {
+            (true, h1.max(a1))
         } else {
-            // Deal with overlap
+            (false, a1)
+        };
+
+        if i == j && !in_right {
+            ranges.insert(i, [a0, a1]);
+        } else {
+            ranges[i] = [new0, new1];
+            for _ in 0..(j - i - ((!in_right) as usize)) {
+                ranges.remove(i + 1);
+            }
         }
     }
 }
@@ -75,8 +82,12 @@ fn part2(input: String) -> TaskResult {
         let a: u64 = a.parse().unwrap();
         let b: u64 = b.parse().unwrap();
 
-        ranges.push([a, b]);
+        insert_new_range(&mut ranges, [a, b]);
     }
 
-    todo!()
+    ranges
+        .into_iter()
+        .map(|[a, b]| b - a + 1)
+        .sum::<u64>()
+        .into()
 }
