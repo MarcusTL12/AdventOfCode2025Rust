e90b799d0b7ea6ac0f80387e592c9310fdd71249
MarcusTL12 <marcuspaafjellet@gmail.com>
2025-12-06 14:36:21 +0100

    part 2

--- src/day6.rs
+++ src/day6.rs
@@ -1,4 +1,4 @@
-use crate::{Day, TaskResult};
+use crate::{Day, TaskResult, util::input_to_grid};
 
 pub const PARTS: Day = [part1, part2];
 
@@ -31,5 +31,39 @@ fn part1(input: String) -> TaskResult {
 }
 
 fn part2(input: String) -> TaskResult {
-    todo!("{input}")
+    let grid = input_to_grid(input.as_bytes());
+    let grid = grid.t();
+
+    let mut op = b'+';
+    let mut ans = 0;
+    let mut cur_res = 0;
+
+    for r in grid.rows() {
+        if r.iter().all(|&x| x == b' ') {
+            ans += cur_res;
+        } else {
+            let mut n = 0;
+            for x in r.iter().filter(|x| x.is_ascii_digit()) {
+                n = 10 * n + (x - b'0') as u64;
+            }
+
+            let &rl = r.last().unwrap();
+
+            match rl {
+                b'+' | b'*' => {
+                    cur_res = n;
+                    op = rl;
+                }
+                _ => match op {
+                    b'+' => cur_res += n,
+                    b'*' => cur_res *= n,
+                    _ => unreachable!(),
+                },
+            }
+        }
+    }
+
+    ans += cur_res;
+
+    ans.into()
 }
